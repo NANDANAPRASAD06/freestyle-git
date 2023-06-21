8069e67aa924881584c274fbf5502c4e02592e36
NANDANAPRASAD06 <54875233+NANDANAPRASAD06@users.noreply.github.com>
2023-06-20 21:33:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+input1 = os.getenv("num")
+print("NUMBER = ", input1)
 parser = argparse.ArgumentParser()
 parser.add_argument("square", help="display a cube of a given number")
 args = parser.parse_args()
